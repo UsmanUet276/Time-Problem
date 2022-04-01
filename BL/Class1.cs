fc82357b7cb6534733805f9af4d6ee70e3562252
usman16371 <usman16371@gmail.com>
2022-04-01 07:30:14 +0500

    Add project files.

---
+++ BL/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.BL
+{
+    internal class Class1
+    {
+        public Class1() { }
+        public Class1(int second, int minutes, int hours)
+        {
+            this.seconds = second;
+            this.minutes = minutes;
+            this.hours = hours;
+        }
+
+        public int seconds;
+        public int minutes;
+        public int hours;
+
+        public int timeInSeconds()
+        {
+            int sec = 0;
+            sec = (hours*3600) + (minutes * 60) + seconds;
+            return sec;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.BL;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int remainingTime = 0;
+            Class1 time1 = new Class1();
+            Class1 time = new Class1(9, 11, 11);
+            int sec = time.timeInSeconds();
+            time1 = inputTime();
+            int sec1 = time1.timeInSeconds();
+            remainingTime = sec1 - sec;
+            Console.WriteLine(remainingTime);
+        }
+        static Class1 inputTime() 
+        { 
+        
+            Class1 temp = new Class1();
+            Console.Write("Hour : ");
+            temp.hours = int.Parse(Console.ReadLine());
+            Console.Write("Minutes : ");
+            temp.minutes = int.Parse(Console.ReadLine());
+            Console.Write("Seconds : ");
+            temp.seconds = int.Parse(Console.ReadLine());
+            return temp;
+        }
+    }
+}
